3b8a146ab07c8b44e69b52c2827159e7c2c9ac15
SuGit-Demo <89434151+SuGit-Demo@users.noreply.github.com>
2022-06-06 21:14:14 +0800

    Create oauth.py

---
+++ oauth.py
@@ -0,0 +1,37 @@
+from flask import Flask,redirect,url_for,render_template
+from flask_dance.contrib.google import make_google_blueprint, google
+
+app = Flask(__name__)
+app.secret_key = 'mysecret'
+#app.config['SECRET_KEY'] = 'mysecret'
+
+blueprint = make_google_blueprint(client_id='479217618451-s9v5792onqdeuolnbuto1vh14vv02b6b.apps.googleusercontent.com',
+                client_secret='secretGOCSPX-_Mu6QsYayqjEGJF1a7UeqV1t0Whz',scope=['profile','email'])
+
+app.registerblueprint(blueprint,url_prefix='/login')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/welcome')
+def welcome():
+    #return internal server error if not logged in
+    resp = google.get('/oauth2/v2/userinfo')
+    assert resp.ok,resp.text
+    email = resp.json()['email']
+    return render_template('welcome.html',email=email)
+
+@app.route('/login/google')
+def login():
+    if not google.authorized:
+        return render_template(url_for('google.login'))
+    #resp = google.get('/oauth2/v2/userinfo')
+    resp = google.get('/plus/v1/people/me')
+    assert resp.ok,resp.text
+    email = resp.json()['email']
+
+    return render_template('welcome.html',email=email)
+
+if __name__ == '__main__':
+    app.run()
